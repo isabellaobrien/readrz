40eb85d78ccf6c62f58db6b762a3b9dc08cd0a32
isabellaobrien <bellaobrien.it@gmail.com>
2024-05-05 23:23:11 +0000

    Create and style SignUp component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>home page</h1>} />
           <Route exact path="/login" render={() => <h1>log in</h1>} />
-          <Route exact path="/signup" render={() => <h1>sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react'
+import {Form, Alert} from 'react-bootstrap'
+import styles from "../../styles/Forms.module.css"
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+    })
+
+    const {username, password1, password2} = signUpData;
+
+    const [errors, setErrors] = useState({})
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.post("/dj-rest-auth/registration/", signUpData);
+            history.push("/login");
+        }catch (err){
+            setErrors(err.response?.data);
+        }
+    }
+  return (
+    <div className={styles.container}>
+        <Form onSubmit={handleSubmit}>
+            <p className={styles.title}>sign up</p>
+            <Form.Group controlId="username" className={styles.input}>
+                <Form.Label className="d-none">username</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="username"
+                    name="username"
+                    value={username} 
+                    onChange={handleChange}
+                />
+
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group controlId="password1" className={styles.input}>
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="password" 
+                    name="password1"
+                    value={password1}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.password1?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group controlId="password2" className={styles.input}>
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="confirm password" 
+                    name="password2"
+                    value={password2}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors.password2?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+
+            <button type="submit" className={styles.btn}>
+                sign up
+            </button>
+            {errors.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+        </Form>
+    </div>
+  )
+}
+
+export default SignUpForm
